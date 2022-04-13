1cb24c6c707bec4ef899161fc8ac74b9c5ba1f2f
Lisa Guevara <Gueval3877@gmail.com>
2022-04-13 09:58:42 -0500

    Initial commit

---
+++ js/FD_script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/*
+ * Feverdream Collective
+ *
+ * Author: Lisa Guevara
+ * Date: 2021-05-11
+ *
+ * Filename: FD_script.js
+ * Used in "Upcoming Events" - FD_upcoming.html
+ */
+
+
+// Alerts user about potential last-minute changes to upcoming events
+window.alert("Due to COVID-19 precautions, all upcoming events are subject to last-minute changes.");
